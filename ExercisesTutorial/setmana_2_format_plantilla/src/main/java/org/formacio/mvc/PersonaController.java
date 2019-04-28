a1bd96ac3573986dfbb982fbff9000ae1a97b492
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-04-28 13:48:58 +0200

    Add same methods to see the differences

--- ExercisesTutorial/setmana_2_format_plantilla/src/main/java/org/formacio/mvc/PersonaController.java
+++ ExercisesTutorial/setmana_2_format_plantilla/src/main/java/org/formacio/mvc/PersonaController.java
@@ -1,9 +1,11 @@
 package org.formacio.mvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.awt.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -29,4 +31,18 @@ public class PersonaController {
 		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
 		return new Persona("Pere","Selva", df.parse("22-12-1980"),"somxulo");
 	}
+
+//	@RequestMapping(path="/persona", produces=MediaType.APPLICATION_JSON_VALUE)
+//	@ResponseBody
+//	public Persona obtePersona() throws ParseException {
+//		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+//		return new Persona("Pere","Selva", df.parse("22-12-1980"),"somxulo");
+//	}
+
+//	@RequestMapping(path="/persona", produces= MediaType.APPLICATION_XML_VALUE)
+//	@ResponseBody
+//	public Persona obtePersona() throws ParseException {
+//		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+//		return new Persona("Pere","Selva", df.parse("22-12-1980"),"somxulo");
+//	}
 }
