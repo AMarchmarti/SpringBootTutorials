8822dd77c2829f8290a6565af549403a8ff60474
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-04-28 14:12:11 +0200

    Second Step pass, run correctly

--- WeekPractices/setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
+++ WeekPractices/setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
@@ -5,6 +5,7 @@ import org.formacio.repositori.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,11 @@ public class AgendaController {
     @ResponseBody
     public String nombreTelefonsAgenda(){
         return String.valueOf(agenda.nombreContactes());
+    }
+
+    @RequestMapping(path = "/telefon")
+    @ResponseBody
+    public String nombreTelefon(@RequestParam(name = "id") String id){
+        return agenda.recupera(id).getTelefon();
     }
 }
