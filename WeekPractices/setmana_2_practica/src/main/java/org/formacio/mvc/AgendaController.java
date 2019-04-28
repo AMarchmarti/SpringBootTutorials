73ab24412734a3ae2cad37687fdaa5e1f714713d
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-04-28 15:20:26 +0200

    Modified a method to handler an exception

--- WeekPractices/setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
+++ WeekPractices/setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
@@ -4,12 +4,10 @@ package org.formacio.mvc;
 import org.formacio.repositori.AgendaService;
 import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 
@@ -31,8 +29,10 @@ public class AgendaController {
         return agenda.recupera(id).getTelefon();
     }
 
+
     @RequestMapping(path = "/contacte/{id}")
     @ResponseBody
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public Persona contacte(@PathVariable String id){
         return agenda.recupera(id);
     }
