7a0e426bc5b145b7a0b95607154a21a74d5dabe3
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-05-05 20:13:23 +0200

    Created the proyect

---
+++ WeekPractices/setmana_3_practica/src/main/java/springbootstarter/CourseApiApp.java
@@ -0,0 +1,12 @@
+package springbootstarter;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+    public static void main(String[] args) {
+
+    }
+}
